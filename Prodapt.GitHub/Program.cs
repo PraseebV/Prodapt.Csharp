2064c03ffd54f813ff6866aceaeb7b5ba46be9bb
praseeb.v <praseeb.v@CLAP1103>
2023-12-14 11:09:37 +0530

    Add project files.

---
+++ Prodapt.GitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Prodapt.GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var today=DateTime.Now;
+            Console.WriteLine($"Today:{today}");
+            Console.WriteLine($"Date Only:{today.ToShortDateString}");
+            Console.WriteLine($"Date Only:{DateTime.Now}");
+        }
+    }
+}
